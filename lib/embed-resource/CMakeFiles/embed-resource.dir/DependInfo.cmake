0a8f5d86d6e24bf5283aacd4efe4302de46a2399
KrutPatel2257 <krutpatel22577@gmail.com>
2019-03-21 14:53:02 -0700

    Library files and Readme.md

---
+++ lib/embed-resource/CMakeFiles/embed-resource.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/krut/Desktop/nanogui_cmpt_464/lib/embed-resource/embedresource.cpp" "/Users/krut/Desktop/nanogui_cmpt_464/lib/embed-resource/CMakeFiles/embed-resource.dir/embedresource.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GLAD_GLAPI_EXPORT"
+  "NANOGUI_PYTHON"
+  "NANOGUI_SHARED"
+  "NVG_SHARED"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  "lib/nanogui/include"
+  "lib/nanogui/ext/glfw/include"
+  "lib/nanogui/ext/nanovg/src"
+  "lib/nanogui/ext/eigen"
+  "lib/embed-resource"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
